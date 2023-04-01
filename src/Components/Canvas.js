820089c77c1a6b1d6d59dfccf4b66ded73bf9b61
Miguel Quirijnen <miguelquirijnen99@gmail.com>
2023-04-01 11:11:39 +0200

    initial message flow

--- src/Components/Canvas.js
+++ src/Components/Canvas.js
@@ -1,7 +1,9 @@
 import React, { useState, useRef, useEffect } from 'react'
-import { Europe } from './Continents';
 
-const DrawingCanvas = props => {
+const WIDTH_VW_FACTOR = 50;
+const HEIGHT_VH_FACTOR = 50;
+
+const Canvas = ({width, height}) => {
   const [drawing, setDrawing] = useState(false);
   const canvasRef = useRef(null);
   const ctxRef = useRef(null);
@@ -9,17 +11,19 @@ const DrawingCanvas = props => {
   useEffect(() => {
     const canvas = canvasRef.current;
     // For supporting computers with higher screen densities, we double the screen density
-    canvas.width = window.innerWidth * 2;
-    canvas.height = window.innerHeight * 2;
+    canvas.width = document.documentElement.clientWidth * (WIDTH_VW_FACTOR/100)
+    canvas.height = document.documentElement.clientHeight * (HEIGHT_VH_FACTOR/100)
+    // canvas.width = window.innerWidth * 2;
+    // canvas.height = window.innerHeight * 2;
     console.log("hw " +  canvas.width + " - " + canvas.height)
-    canvas.style.width = `${window.innerWidth}px`;
-    canvas.style.height = `${window.innerHeight}px`;
+    canvas.style.width = `${WIDTH_VW_FACTOR}vw`;
+    canvas.style.height = `${HEIGHT_VH_FACTOR}vh`;
     // Setting the context to enable us draw
     const ctx = canvas.getContext('2d');
-    ctx.scale(2, 2);
+    // ctx.scale(2, 2);
     ctx.lineCap = 'round';
     ctx.strokeStyle = 'black';
-    ctx.lineWidth = 5;
+    ctx.lineWidth = 3;
     ctxRef.current = ctx;
   }, []);
 
@@ -55,9 +59,24 @@ const DrawingCanvas = props => {
         onMouseUp={stopDraw}
         onMouseMove={draw}
         ref={canvasRef}
-      ><Europe /></canvas>
+        style={canvasStyle}
+        // width={"50vw"}
+        // height={"70vh"}
+      />
     </>
   );
 }
 
-export default DrawingCanvas
+const canvasStyle = {
+  border: "1 px solid black",
+  backgroundColor: "rgba(255,255,255,0.5)",
+  position: "absolute",
+  width: `${WIDTH_VW_FACTOR}vw`,
+  height: `${HEIGHT_VH_FACTOR}vh`,
+  top: "50%",
+  left: "50%",
+  marginRight: "-50%",
+  transform: "translate(-50%, -50%)"
+}
+
+export default Canvas;
--- src/Worldmap.js
+++ src/Worldmap.js
@@ -9,43 +9,74 @@ import {
   MiddleEast,
   NorthAmerica,
 } from "./Continents";
-import { IADMFR_LOGO } from "./logo";
+import { IADMFR_LOGO } from "./Logo";
 import { useState } from "react";
+import Canvas from "./Components/Canvas";
 
 // Declaration of all the constants
 const BASE_VIEWBOX = "0 0 2000 857"
 const ANIMATION_DURATION = 0.5 // in seconds
 
+const steps = {
+  continentSelection: 0,
+  continentConfirmation: 1,
+  messageDrawing: 2,
+  messagePlacing: 3,
+}
+
 function Worldmap() {
   const [viewBox, setViewBox] = useState(BASE_VIEWBOX)
   const [drawingMode, setDrawingMode] = useState(false)
+  const [msgInProgress, setMsgInProgress] = useState(false)
   const [currentContinent, setCurrentContinent] = useState("");
+  const [currentStep, setCurrentStep] = useState(steps.continentSelection)
 
   const svgRef = useRef(null);
 
   const handleContinentClick = async (e) => {
-    if (drawingMode) return;
+    if (currentStep !== steps.continentSelection) return;
 
     const gBox = e.currentTarget.getBBox();
     const newViewBox = `${gBox.x} ${gBox.y} ${gBox.width} ${gBox.height}`;
 
     gsap.to(svgRef.current, { duration: ANIMATION_DURATION, attr: { viewBox: newViewBox } });
 
-    // Enable drawing
-    setDrawingMode(true);
+    // Start msg 
+    setCurrentStep(steps.continentConfirmation)
 
     setCurrentContinent(e.currentTarget.id);
   }
 
+  const handleConfirmContinentClick = (e) => {
+    // Disable drawing
+    setCurrentStep(steps.messageDrawing);
+    setDrawingMode(true);
+
+  }
+
+  const handleConfirmMessageClick  = (e) => {
+    // Disable drawing
+    setDrawingMode(false);
+    setCurrentStep(steps.messagePlacing);
+  }
+
+  const handleConfirmPlacementClick = (e) => {
+    setCurrentStep(steps.continentSelection);
+
+    gsap.to(svgRef.current, { duration: ANIMATION_DURATION, attr: { viewBox: BASE_VIEWBOX } });
+  }
+
+
   const handleReturnClick = (e) => {
     // Disable drawing
     setDrawingMode(false);
+    setMsgInProgress(false);
+    setCurrentStep(steps.continentSelection)
+    setCurrentContinent("");
 
     gsap.to(svgRef.current, { duration: ANIMATION_DURATION, attr: { viewBox: BASE_VIEWBOX } });
-
-    setCurrentContinent("");
   }
-
+  
   return (
     <div style={{ overflow: "hidden" }}>
       <svg
@@ -82,18 +113,43 @@ function Worldmap() {
       <div className="logoBox">
         <IADMFR_LOGO />
       </div>
-      {drawingMode && <button className="returnButton" onClick={(e) => handleReturnClick(e)} >Return</button>}
+      {currentStep === steps.continentConfirmation && <>
+        <a style={confirmationTextStyle}>{`Confirm ${currentContinent}?`}</a>
+        <button className="confirmContinentButton" onClick={(e) => handleConfirmContinentClick(e)} >Confirm continent</button>
+        <button className="returnButton" onClick={(e) => handleReturnClick(e)} >Return</button>
+      </>}
+      {currentStep === steps.messageDrawing && <>
+        <Canvas width={"200px"} height={"200px"} />
+        <button className="confirmMessageButton" onClick={(e) => handleConfirmMessageClick(e)} >Confirm Message</button>
+        <button className="returnButton" onClick={(e) => handleReturnClick(e)} >Return</button>
+      </>}
+      {currentStep === steps.messagePlacing && <>
+        <a style={confirmationTextStyle}>{`Place your message on the continent.`}</a>
+        <button className="confirmContinentButton" onClick={(e) => handleConfirmPlacementClick(e)} >Confirm placement</button>
+        <button className="returnButton" onClick={(e) => handleReturnClick(e)} >Return</button>
+      </>}
 
       {/* TODO REMOVE */}
-      <a style={{ position: "absolute", bottom: "10px", right: "600px", width: "300px", height: "80px" }}>
+      <a style={{ position: "absolute", bottom: "10px", right: "600px", width: "300px", height: "100px" }}>
         {`Drawingmode: ${drawingMode} `}
         <br />
         {` ViewBox: ${viewBox}`}
         <br />
         {` Continent: ${currentContinent}`}
+        <br />
+        {` Step: ${currentStep}`}
       </a>
     </div>
   );
+}
+
+const confirmationTextStyle = {
+  color: "white",
+  fontSize: "large",
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  marginRight: "-50%",
 }
 
 export default Worldmap;
